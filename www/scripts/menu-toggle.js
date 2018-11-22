5b1d5ef13307e145b64f3dc9315f035d92dad466
Clive Anthony Duncan <cad324@cornell.edu>
2018-11-21 23:56:10 -0500

    made js mobile responsive

--- www/scripts/menu-toggle.js
+++ www/scripts/menu-toggle.js
@@ -1,7 +1,12 @@
 $(document).ready(function() {
+  
+  if ($(window).width() < 1000) {
+    $("nav ul").addClass("hidden");
+  }
 
   $(".menu-icon").click(function() {
     $(".menu-icon").toggleClass("switch");
+    $("nav ul").toggleClass("hidden");
   });
 
 });
