04a561cfe91bc334737f7b8db175d756050de2c4
Clive Anthony Duncan <cad324@cornell.edu>
2018-11-22 01:31:55 -0500

    removed debugging console log

--- www/scripts/menu-toggle.js
+++ www/scripts/menu-toggle.js
@@ -8,7 +8,6 @@ $(document).ready(function() {
   // Event listener, called when window width changes
   $(window).resize(function() {
     //Condition to handle when to show show/hide nav items based on window/device width
-    console.log($(window).width() < 1000 && !$("nav ul").hasClass("hidden") && !$(".menu-icon").hasClass("switch"));
     if ($(window).width() < 1000 && !$("nav ul").hasClass("hidden") && !$(".menu-icon").hasClass("switch")) {
       $("nav ul").addClass("hidden");
     } else if ($(window).width() > 1000 && $("nav ul").hasClass("hidden")) {
