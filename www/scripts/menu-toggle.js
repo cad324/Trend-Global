507238cd9200e5fffdf35d8d65c7207298bdf914
Clive Anthony Duncan <cad324@cornell.edu>
2018-11-22 01:10:09 -0500

    condition for when to show/hide nav items based on device width

--- www/scripts/menu-toggle.js
+++ www/scripts/menu-toggle.js
@@ -1,8 +1,19 @@
 $(document).ready(function() {
-  
+
+  /* TODO: Prevent nav items from flashing when page loads */
   if ($(window).width() < 1000) {
     $("nav ul").addClass("hidden");
   }
+
+  // Event listener, called when window width changes
+  $(window).resize(function() {
+    //Condition to handle when to show show/hide nav items based on window/device width
+    if ($(window).width() < 1000 && !$("nav ul").hasClass("hidden")) {
+      $("nav ul").addClass("hidden");
+    } else if ($(window).width() >= 1000 && $("nav ul").hasClass("hidden")) {
+      $("nav ul").removeClass("hidden");
+    }
+  });
 
   $(".menu-icon").click(function() {
     $(".menu-icon").toggleClass("switch");
