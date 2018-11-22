1c2b2feb12a264fa7bf566e5707056cf44132bc7
Clive Anthony Duncan <cad324@cornell.edu>
2018-11-22 01:59:50 -0500

    fixed menu toggle bug

--- www/scripts/menu-toggle.js
+++ www/scripts/menu-toggle.js
@@ -1,23 +1,24 @@
 $(document).ready(function() {
 
-  /* TODO: Prevent nav items from flashing when page loads */
-  if ($(window).width() < 1000) {
-    $("nav ul").addClass("hidden");
-  }
-
   // Event listener, called when window width changes
   $(window).resize(function() {
     //Condition to handle when to show show/hide nav items based on window/device width
-    if ($(window).width() < 1000 && !$("nav ul").hasClass("hidden") && !$(".menu-icon").hasClass("switch")) {
-      $("nav ul").addClass("hidden");
-    } else if ($(window).width() > 1000 && $("nav ul").hasClass("hidden")) {
-      $("nav ul").removeClass("hidden");
+    if ($(window).width() < 1000 && !$(".menu-icon").hasClass("switch")) {
+      $("nav ul").css("display", "none");
+    } else if ($(window).width() > 1000) {
+      $("nav ul").css("display", "flex");
+    } else {
+      $("nav ul").css("display", "block");
     }
   });
 
   $(".menu-icon").click(function() {
     $(".menu-icon").toggleClass("switch");
-    $("nav ul").toggleClass("hidden");
+    if ($(".menu-icon").hasClass("switch")) {
+      $("nav ul").css("display", "block");
+    } else {
+      $("nav ul").css("display", "none");
+    }
   });
 
 });
