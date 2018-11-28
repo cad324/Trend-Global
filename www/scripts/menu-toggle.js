89316accc2a749d3cbe305de85a76dbccb43d066
Clive Anthony Duncan <cad324@cornell.edu>
2018-11-28 11:42:22 -0500

    replaced .css() with addClass()/removeClass() so we don't lose points

--- www/scripts/menu-toggle.js
+++ www/scripts/menu-toggle.js
@@ -4,20 +4,30 @@ $(document).ready(function() {
   $(window).resize(function() {
     //Condition to handle when to show show/hide nav items based on window/device width
     if ($(window).width() <= 1000 && !$(".menu-icon").hasClass("switch")) {
-      $("nav ul").css("display", "none");
+      $("nav ul").addClass("hidden");
+      $("nav ul").removeClass("block");
+      $("nav ul").removeClass("flex");
     } else if ($(window).width() > 1000) {
-      $("nav ul").css("display", "flex");
+      $("nav ul").addClass("flex");
+      $("nav ul").removeClass("hidden");
+      $("nav ul").removeClass("block");
     } else {
-      $("nav ul").css("display", "block");
+      $("nav ul").addClass("block");
+      $("nav ul").removeClass("hidden");
+      $("nav ul").removeClass("flex");
     }
   });
 
   $(".menu-icon").click(function() {
     $(".menu-icon").toggleClass("switch");
     if ($(".menu-icon").hasClass("switch")) {
-      $("nav ul").css("display", "block");
+      $("nav ul").addClass("block");
+      $("nav ul").removeClass("hidden");
+      $("nav ul").removeClass("flex");
     } else {
-      $("nav ul").css("display", "none");
+      $("nav ul").addClass("hidden");
+      $("nav ul").removeClass("block");
+      $("nav ul").removeClass("flex");
     }
   });
 
