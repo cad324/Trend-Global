7b992cf9889427d2c19820a5e65c99b0ff99d224
David Mwanjabala <dm592@cornell.edu>
2018-11-28 21:19:36 -0500

    rearranged kids page and changed background to grey

--- www/scripts/hover_captions.js
+++ www/scripts/hover_captions.js
@@ -5,28 +5,28 @@ $(document).ready(function() {
 
    //image1 - caption1
      $(images[0]).mouseenter(function(){
-       $(captions[0]).show();
+       $(captions[0]).fadeIn();
      });
      $(images[0]).mouseleave(function(){
        $(captions[0]).hide();
      });
   //image2 - caption2
      $(images[1]).mouseenter(function(){
-       $(captions[1]).show();
+       $(captions[1]).fadeIn();
      });
      $(images[1]).mouseleave(function(){
        $(captions[1]).hide();
      });
  //image3 - caption3
     $(images[2]).mouseenter(function(){
-      $(captions[2]).show();
+      $(captions[2]).fadeIn();
     });
     $(images[2]).mouseleave(function(){
       $(captions[2]).hide();
     });
 //image4 - caption4
    $(images[3]).mouseenter(function(){
-     $(captions[3]).show();
+     $(captions[3]).fadeIn();
    });
    $(images[3]).mouseleave(function(){
      $(captions[3]).hide();
@@ -34,7 +34,7 @@ $(document).ready(function() {
 
 //image5 - caption5
   $(images[4]).mouseenter(function(){
-    $(captions[4]).show();
+    $(captions[4]).fadeIn();
   });
   $(images[4]).mouseleave(function(){
     $(captions[4]).hide();
@@ -42,7 +42,7 @@ $(document).ready(function() {
 
 //image6 - caption6
    $(images[5]).mouseenter(function(){
-     $(captions[5]).show();
+     $(captions[5]).fadeIn();
    });
    $(images[5]).mouseleave(function(){
      $(captions[5]).hide();
